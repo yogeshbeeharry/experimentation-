2f4d9f0a27dcf99eb2fbe38cf14c8dadf415d32a
yogeshbeeharry <141336645+yogeshbeeharry@users.noreply.github.com>
2024-04-03 10:39:53 +0400

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import LogisticRegression
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
